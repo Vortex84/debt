bac45d879bc711a6c9f5cdbe873cd3ff15cc9f30
Vortex84 <storm77784@mail.ru>
2017-11-13 14:37:25 +0300

    ok

---
+++ app/Models/Blank_type.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Blank_type extends Model
+{
+    protected $table = 'debt_blank_type';
+    protected $primaryKey = 'idbt';
+
+
+
+
+}
